e8bf2f0bc8f4a0119412c9f1b74f640b950e1fc3
Kalbande123 <saibabaaniaaibaba@gmail.com>
2026-08-13 21:27:08 +0530

    Update SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // तुमची गुगल शीटची Apps Script URL येथे तशीच राहू द्या
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxjNAUe6wW9VguqrJl_lXZGI2IgmfMZG2c5GAxugvrVNgEM9mahBuHMRYha6zAGh6__6Q/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbwTgiFopUUEAmY0GOWNLMwnrPfsLajvXyDbam8UuxF467wX2jiTFtkiEzp4s5E38pq9Vg/exec/exec",
     
     // घोषणापत्रासाठी लागणारे टोकन (येथे आपण २ सेट करत आहोत)
     GHOSHANA_TOKEN_COST: 2 
