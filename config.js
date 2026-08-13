592da05263145c5c87d5cc31565b97b75ee991eb
Kalbande123 <saibabaaniaaibaba@gmail.com>
2026-08-13 21:44:42 +0530

    Update SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // तुमची नवीन Google Apps Script Web App URL
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwTgiFopUUEAmY0GOWNLMwnrPfsLajvXyDbam8UuxF467wX2jiTFtkiEzp4s5E38pq9Vg/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxMGHJh670aoUPwA-j3SwyQMZ-yzSbGS7VSXeSvAWloNaj9znLNHOSRFi0FMUxuQUVBIQ/exec",
     
     // घोषणापत्रासाठी लागणारे टोकन
     GHOSHANA_TOKEN_COST: 2 
