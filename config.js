e5303c8ed38995d4039fc0ae4bfcfcd94a5c68ac
Kalbande123 <saibabaaniaaibaba@gmail.com>
2026-08-13 12:34:46 +0530

    Change SCRIPT_URL in config.js
    Updated the SCRIPT_URL for Google Sheets Apps Script.
    admin wali appscript ai update keli

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // तुमची गुगल शीटची Apps Script URL येथे तशीच राहू द्या
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxq9pgRz5rqZSNp4EefQnHzXPKE_KulFmlCgaGd_dgV_IuIlCB2tamMcLF4I3W09gNk9A/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxjNAUe6wW9VguqrJl_lXZGI2IgmfMZG2c5GAxugvrVNgEM9mahBuHMRYha6zAGh6__6Q/exec",
     
     // घोषणापत्रासाठी लागणारे टोकन (येथे आपण २ सेट करत आहोत)
     GHOSHANA_TOKEN_COST: 2 
