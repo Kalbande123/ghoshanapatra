d70a0c6927ae4b57499c4e98326dd52d607571be
Kalbande123 <saibabaaniaaibaba@gmail.com>
2026-08-19 13:47:46 +0530

    Change SCRIPT_URL in config.js
    Updated SCRIPT_URL to a new Google Apps Script Web App URL.
    5 id enter

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // तुमची नवीन Google Apps Script Web App URL
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz4ejVUZmTPJw2AcxBwZ9pPAExuIVQFqDsLB-ySBNJ1ILi21nLelvEYKq6dZcHTzMm_DA/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyCufHmNbkPnO7cWfElZ78QEl8bE9SKLUkiY3_vlVi4H0XGwfiwp1SEzGNhnXj-4Yy3QQ/exec",
     
     // घोषणापत्रासाठी लागणारे टोकन
     GHOSHANA_TOKEN_COST: 2 
