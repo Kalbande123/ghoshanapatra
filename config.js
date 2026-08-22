920ec31b43f356bf07342977715a4d603beae3f9
Kalbande123 <saibabaaniaaibaba@gmail.com>
2026-08-22 18:37:32 +0530

    maha id add

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // तुमची नवीन Google Apps Script Web App URL
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyCufHmNbkPnO7cWfElZ78QEl8bE9SKLUkiY3_vlVi4H0XGwfiwp1SEzGNhnXj-4Yy3QQ/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwxlyOp3OuQ48gHjOEGRn2Yi-SObKJb2tcrgZftj5RDewRS_Deg5ozVg-w0LCKPJqFwzA/exec",
     
     // घोषणापत्रासाठी लागणारे टोकन
     GHOSHANA_TOKEN_COST: 2 
